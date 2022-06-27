d54878c616eaa0d50794686b073a5e15d244e432
speedy <yann-arthur.tcheumani@epitech.eu>
2022-06-27 22:47:02 +0200

    [FIX] read

--- Protocol/test.py
+++ Protocol/test.py
@@ -3,3 +3,5 @@ from protocol import API, CMD
 api = API()
 response = api.write(CMD.SETUP)
 print(f"j'ai recu ceci {response.data}")
+response = api.write(CMD.BATTERIE)
+print(f"j'ai recu ceci {response.data}")
