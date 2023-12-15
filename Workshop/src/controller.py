3de2349b107feda69ba5219e2d187a283e60538a
Celian A <celian.aldehuelo@gmail.com>
2023-12-15 18:43:07 +0100

    Add comments

--- Workshop/src/controller.py
+++ Workshop/src/controller.py
@@ -37,16 +37,27 @@ def float_point_to_int(point: tuple[float,float]) -> tuple[int,int]:
 
 
 def setup(input_video: MemoryVideoProvider, output_video: MemoryVideoProvider, serial: SerialDevice):
-    pass
+    pass # Ecrivez ici le code d'initialization (executer une unique fois au début)
 
 
 def loop(input_video: MemoryVideoProvider, output_video: MemoryVideoProvider, serial: SerialDevice):
+    # Le code ici est exécuté en boucle
+
     frame = input_video.read()
     if frame is None:
         return True
 
     splashs = detector.detect(frame)
     splashs.render(frame)
+
+    # Chaque splash represente une "tache" de couleur détecter sur l'écran
+
+    for splash in splashs.splashs:
+        # splash.area : Nombre de pixel dans la tache
+        # splash.centroid : Un tuple (x, y) qui est les coordonée du centre de la tache
+        # splash.box : Un tuple (left, top, right, bottom) qui est la boite qui encadre la tache
+        # splash.color : Qui est à soit RED_COLOR_RANGE ou à GREEN_COLOR_RANGE
+        pass
 
     output_video.write(frame)
 
